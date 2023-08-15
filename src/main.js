75d233a90dfd6a705f621a55ab3c6658a8686c1a
Renan Martins <91997504+nanmartins@users.noreply.github.com>
2023-08-15 14:10:59 +0100

    setting mdi icons

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@
  * Bootstraps Vuetify and other plugins then mounts the App`
  */
 
+
 // Components
 import App from './App.vue'
 
@@ -12,6 +13,7 @@ import { createApp } from 'vue'
 
 // Plugins
 import { registerPlugins } from '@/plugins'
+
 
 const app = createApp(App)
 
