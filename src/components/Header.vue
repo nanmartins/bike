f07380a61e5df6839ef66a38090757b446d33ec3
Renan Martins <91997504+nanmartins@users.noreply.github.com>
2023-08-15 05:03:06 +0100

    need to crop the logo img to avoid some spaces

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,21 @@
 <template>
   <!-- <div class="bg-grey-darken-4"> -->
-    <v-container>
+    <v-container class="py-0">
       <v-responsive class="align-center text-start">
         <div class="d-flex flex-row align-center justify-space-between">
-          <h1>Nav</h1>
+          <!-- <h1>Nav</h1> -->
+          <v-img
+            :src="bikeLogo"
+            max-width="100px"
+            class="d-flex justify-start pa-0 ma-0 justify-start"
+          ></v-img>
           <h2>Menu</h2>
         </div>
       </v-responsive>
     </v-container>
   <!-- </div> -->
 </template>
+
+<script setup>
+import bikeLogo from  '@/assets/bike-logo.png'
+</script>
